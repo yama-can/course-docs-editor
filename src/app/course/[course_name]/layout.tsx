b93472efc09b0bd0ebff84f6619d1dc89452a062
yama.can <yama.can95@gmail.com>
2025-04-02 12:34:34 +0900

    [update] Add course layout header

--- src/app/course/[course_name]/layout.tsx
+++ src/app/course/[course_name]/layout.tsx
@@ -7,6 +7,14 @@ export default async function CourseLayout({ children, params }: { children: Rea
 	return (
 		<div style={{ minWidth: "400px", width: "55%", overflowWrap: "break-word", margin: "0 auto" }} className="course-layout">
 
+			<div className="course-layout-header">
+
+				<a href={`/course/${course_name}/milestone`}>
+					教材一覧
+				</a>
+
+			</div>
+
 			{children}
 
 		</div>
